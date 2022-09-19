5cbca5932b621cce705e161cab005f3f64a8b88a
Ömer GENÇ <sweethungri@gmail.com>
2022-09-19 16:16:20 +0300

    Side branch

--- lib/flash_chat_to_migrate/flash_chat_to_apply_later/registration_screen.dart
+++ lib/flash_chat_to_migrate/flash_chat_to_apply_later/registration_screen.dart
@@ -1,6 +1,5 @@
 import 'package:chat_new/flash_chat_to_migrate/flash_chat_to_apply_later/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
